6d41bec4e7679e26a7571a5f1b79b27a824bb003
mypiroglu63 <mypiroglu63@gmail.com>
2024-06-13 16:33:44 +0300

    Addeed

--- netflix-new/src/App.jsx
+++ netflix-new/src/App.jsx
@@ -1,7 +1,40 @@
+import { Link, Route, Switch } from "react-router-dom/cjs/react-router-dom.min";
 import "./App.css";
+import Login from "./pages/Login";
+import Welcome from "./pages/Welcome";
+import Home from "./pages/Home";
+import Header from "./components/Header/Header";
+import { useState } from "react";
 
 function App() {
-  return <>Yasin</>;
+  const [activeProfile, setActiveProfile] = useState(null);
+
+  return (
+    <>
+      <Switch>
+        <Route exact path="/">
+          <h1>Welcome to Netflix by Wit</h1>
+        </Route>
+        <Route path="/login">
+          <Header />
+          <Login />
+        </Route>
+        <Route path="/welcome">
+          <Welcome setActiveProfile={setActiveProfile} />
+        </Route>
+        <Route path="/home">
+          <Header activeProfile={activeProfile} />
+          <Home activeProfile={activeProfile} />
+        </Route>
+      </Switch>
+      <div className="dev-navigation">
+        <Link to="/home">Home</Link>
+        <Link to="/login">Login</Link>
+        <Link to="/welcome">Welcome</Link>
+        <Link to="/">HomePage</Link>
+      </div>
+    </>
+  );
 }
 
 export default App;
